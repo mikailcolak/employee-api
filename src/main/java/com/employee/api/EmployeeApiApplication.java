dbbedea9f7d30fd1395a7ca00fe61c8afe47524b
Mikail ÇOLAK <mikailcolak@gmail.com>
2020-09-27 00:08:33 +0300

    Open API configured.

--- src/main/java/com/employee/api/EmployeeApiApplication.java
+++ src/main/java/com/employee/api/EmployeeApiApplication.java
@@ -1,7 +1,14 @@
 package com.employee.api;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EmployeeApiApplication {
@@ -10,4 +17,16 @@ public class EmployeeApiApplication {
 		SpringApplication.run(EmployeeApiApplication.class, args);
 	}
 
+	@Bean
+	public OpenAPI customOpenAPI(@Value("${springdoc.version}") String appVersion) {
+		return new OpenAPI()
+			.components(new Components())
+			.info(new Info()
+		        .title("Employee API")
+		        .version(appVersion)
+		        .license(new License().name("MIT").url("http://localhost:8080"))
+			);
+	}
+
 }
+
