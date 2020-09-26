b79548a49706212e7b957998191843e3d3d23d44
Mikail ÇOLAK <mikailcolak@gmail.com>
2020-09-27 00:14:29 +0300

    Model Assembler created: CompanyModelAssembler, EmployeeModelAssembler

---
+++ src/main/java/com/employee/api/controller/assembler/CompanyModelAssembler.java
@@ -0,0 +1,26 @@
+package com.employee.api.controller.assembler;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+import com.employee.api.controller.CompanyController;
+import com.employee.api.model.Company;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CompanyModelAssembler implements RepresentationModelAssembler<Company, EntityModel<Company>> {
+
+    @Override
+    public EntityModel<Company> toModel(Company company) {
+        try {
+            return EntityModel.of(
+                company,
+                linkTo(methodOn(CompanyController.class).one(company.getId())).withSelfRel(),
+                linkTo(methodOn(CompanyController.class).all()).withRel("companies")
+            );
+        } catch (Exception ex) {
+            return EntityModel.of(company);
+        }
+    }
+}
