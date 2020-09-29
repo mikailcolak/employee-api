7be222d1fd4bd37cd12ce4f0fe1d8f0707f85260
Mikail ÇOLAK <mikailcolak@gmail.com>
2020-09-29 20:45:10 +0300

    DTO created as GenericResponse for generic API messages.

---
+++ src/main/java/com/employee/api/model/GenericResponse.java
@@ -0,0 +1,19 @@
+package com.employee.api.model;
+
+public class GenericResponse {
+
+    private String message;
+
+    public GenericResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
