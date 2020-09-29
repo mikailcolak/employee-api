4111cfb8b1c2a74d72666b6ee778bb6c9abe47b4
Mikail ÇOLAK <mikailcolak@gmail.com>
2020-09-29 18:00:57 +0300

    EmployeeRepository::findAllByCompanyId signature changed to accept Pageable.

--- src/main/java/com/employee/api/repository/EmployeeRepository.java
+++ src/main/java/com/employee/api/repository/EmployeeRepository.java
@@ -1,14 +1,14 @@
 package com.employee.api.repository;
 
 import com.employee.api.model.Employee;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
     @Query("SELECT e FROM Employee e WHERE e.companyId = ?1")
-    List<Employee> findAllByCompanyId(Long id);
+    Page<Employee> findAllByCompanyId(Long id, Pageable pageable);
 
 }
