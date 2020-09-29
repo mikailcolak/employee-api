7bac40114584793b53f30f74d18ee220178f797f
Mikail ÇOLAK <mikailcolak@gmail.com>
2020-09-29 18:07:49 +0300

    API Exception handlers fixed and returns an object instead of plain text.

--- src/main/java/com/employee/api/controller/advice/CompanyNotFoundAdvice.java
+++ src/main/java/com/employee/api/controller/advice/CompanyNotFoundAdvice.java
@@ -1,19 +1,21 @@
 package com.employee.api.controller.advice;
 
 import com.employee.api.controller.exception.CompanyNotFoundException;
+import com.employee.api.model.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-class CompanyNotFoundAdvice {
+class CompanyNotFoundAdvice extends ResponseEntityExceptionHandler {
 
     @ResponseBody
     @ExceptionHandler(CompanyNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    String employeeNotFoundHandler(CompanyNotFoundException ex) {
-        return ex.getMessage();
+    Object companyNotFoundHandler(CompanyNotFoundException ex) {
+        return new ErrorResponse(ex.getMessage());
     }
 }
--- src/main/java/com/employee/api/controller/advice/EmployeeNotFoundAdvice.java
+++ src/main/java/com/employee/api/controller/advice/EmployeeNotFoundAdvice.java
@@ -1,19 +1,21 @@
 package com.employee.api.controller.advice;
 
 import com.employee.api.controller.exception.EmployeeNotFoundException;
+import com.employee.api.model.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-class EmployeeNotFoundAdvice {
+class EmployeeNotFoundAdvice extends ResponseEntityExceptionHandler {
 
     @ResponseBody
     @ExceptionHandler(EmployeeNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    String employeeNotFoundHandler(EmployeeNotFoundException ex) {
-        return ex.getMessage();
+    Object employeeNotFoundHandler(EmployeeNotFoundException ex) {
+        return new ErrorResponse(ex.getMessage());
     }
 }
