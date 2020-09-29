2bf1e6277148df383c07967ef480da836680fd20
Mikail ÇOLAK <mikailcolak@gmail.com>
2020-09-29 23:04:07 +0300

    Smoke test for controllers written inside of contextLoads test method.

--- src/test/java/com/employee/api/EmployeeApiApplicationTests.java
+++ src/test/java/com/employee/api/EmployeeApiApplicationTests.java
@@ -1,13 +1,31 @@
 package com.employee.api;
 
+import com.employee.api.controller.CompanyController;
+import com.employee.api.controller.DefaultController;
+import com.employee.api.controller.EmployeeController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class EmployeeApiApplicationTests {
 
+	@Autowired
+	private DefaultController defaultController;
+
+	@Autowired
+	private CompanyController companyController;
+
+	@Autowired
+	private EmployeeController employeeController;
+
 	@Test
 	void contextLoads() {
+		assertThat(defaultController).isNotNull();
+		assertThat(companyController).isNotNull();
+		assertThat(employeeController).isNotNull();
 	}
 
 }
