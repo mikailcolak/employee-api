bc04a22d04aeb1b18b0c57d91a292a1d22f1a0e5
Mikail ÇOLAK <mikailcolak@gmail.com>
2020-10-07 15:36:21 +0300

    Method declared in EmployeeRepository: deleteAllByCompanyId(Long id)

--- src/main/java/com/employee/api/repository/EmployeeRepository.java
+++ src/main/java/com/employee/api/repository/EmployeeRepository.java
@@ -4,7 +4,9 @@ import com.employee.api.model.Employee;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
@@ -16,5 +18,10 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
     @Query("SELECT AVG(e.salary) FROM Employee e WHERE e.companyId = ?1")
     float averageSalaryByCompanyId(Long id);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Employee WHERE companyId = ?1")
+    void deleteAllByCompanyId(Long id);
 
 }
