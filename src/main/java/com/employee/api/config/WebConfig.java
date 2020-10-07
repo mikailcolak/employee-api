3fe69c16e5b293ba555718693b5b4af3799baeef
Mikail ÇOLAK <mikailcolak@gmail.com>
2020-10-07 15:34:30 +0300

    CORS settings ["GET", "POST", "PUT", "DELETE] methods allowed.

--- src/main/java/com/employee/api/config/WebConfig.java
+++ src/main/java/com/employee/api/config/WebConfig.java
@@ -12,7 +12,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry
             .addMapping("/**")
-            .allowedOrigins("http://localhost:3000") // allow react js default address:port on local env.
+            .allowedOrigins("http://localhost:3000")
+            .allowedMethods("GET", "POST","PUT", "DELETE"); // allow react js default address:port on local env.
         ;
     }
 
