a262468aad0d815f63656ab5afb0d43e95e963e1
Mikail ÇOLAK <mikailcolak@gmail.com>
2020-10-07 17:12:55 +0300

    Revert "Initial data ids updated."
    This reverts commit a599941733e8da1f9d0efed3832ed02e05968e76.

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -1,12 +1,10 @@
-TRUNCATE TABLE companies;
 INSERT INTO companies
 (id, name)
 VALUES
-(10, 'EBF');
+(1, 'EBF');
 
-TRUNCATE TABLE employees;
 INSERT INTO employees
 (id, company_id, name, surname, email, address, salary)
 VALUES
-(10, 10, 'TEST1', 'TEST1_SURNAME', 'test1@employeeapi.com', 'ADDRESS1', 4567),
-(11, 10, 'TEST2', 'TEST2_SURNAME', 'test2@employeeapi.com', 'ADDRESS2', 4598);
+(1, 1, 'TEST1', 'TEST1_SURNAME', 'test1@employeeapi.com', 'ADDRESS1', 4567),
+(2, 1, 'TEST2', 'TEST2_SURNAME', 'test2@employeeapi.com', 'ADDRESS2', 4598);
