a599941733e8da1f9d0efed3832ed02e05968e76
Mikail ÇOLAK <mikailcolak@gmail.com>
2020-10-07 15:39:22 +0300

    Initial data ids updated.

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -1,10 +1,12 @@
+TRUNCATE TABLE companies;
 INSERT INTO companies
 (id, name)
 VALUES
-(1, 'EBF');
+(10, 'EBF');
 
+TRUNCATE TABLE employees;
 INSERT INTO employees
 (id, company_id, name, surname, email, address, salary)
 VALUES
-(1, 1, 'TEST1', 'TEST1_SURNAME', 'test1@employeeapi.com', 'ADDRESS1', 4567),
-(2, 1, 'TEST2', 'TEST2_SURNAME', 'test2@employeeapi.com', 'ADDRESS2', 4598);
+(10, 10, 'TEST1', 'TEST1_SURNAME', 'test1@employeeapi.com', 'ADDRESS1', 4567),
+(11, 10, 'TEST2', 'TEST2_SURNAME', 'test2@employeeapi.com', 'ADDRESS2', 4598);
